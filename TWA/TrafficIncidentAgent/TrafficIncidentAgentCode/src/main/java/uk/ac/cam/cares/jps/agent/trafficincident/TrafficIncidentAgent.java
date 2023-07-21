f801d83b0f7c18e385bac72ae9ca6cecd49c92e2
Echomo-Xinyu <seanxinyu2@gmail.com>
2023-07-21 17:24:45 +0800

    standardize start time and end time format

--- TWA/TrafficIncidentAgent/TrafficIncidentAgentCode/src/main/java/uk/ac/cam/cares/jps/agent/trafficincident/TrafficIncidentAgent.java
+++ TWA/TrafficIncidentAgent/TrafficIncidentAgentCode/src/main/java/uk/ac/cam/cares/jps/agent/trafficincident/TrafficIncidentAgent.java
@@ -27,7 +27,7 @@ public class TrafficIncidentAgent implements Runnable {
     public static final String CONNECTOR_ERROR_MSG = "Error when working with APIConnector.";
     
     public static final ZoneOffset offset= ZoneOffset.UTC;
-    long timestamp = System.currentTimeMillis();
+    OffsetDateTime timestamp = OffsetDateTime.now(TrafficIncidentAgent.offset).toInstant().getEpochSecond();
     private HashSet<TrafficIncident> ongoingTrafficIncidentSet = new HashSet<>();
     private HashSet<TrafficIncident> newTrafficIncidentSet = new HashSet<>();
 
@@ -76,7 +76,7 @@ public class TrafficIncidentAgent implements Runnable {
         while (true) {
             
             // timestamp records current time to get data from API
-            this.timestamp = System.currentTimeMillis();
+            this.timestamp = OffsetDateTime.now(TrafficIncidentAgent.offset).toInstant().getEpochSecond();
             readings = connector.getReadings();
             
             if (readings == null) {
